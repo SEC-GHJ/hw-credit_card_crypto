80f09f02eeb8bd5d5b2bdf6eee9f75b0c81244d8
June Hsu <junehsu920179@gmail.com>
2022-03-14 16:33:10 +0800

    Finish substitution cipher with Caesar&Permutation

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -6,59 +6,59 @@ require_relative '../double_trans_cipher'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
-  # before do
-  #   @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
-  #                        'Soumya Ray', 'Visa')
-  #   @key = 3
-  # end
-
-  # describe 'Using Caesar cipher' do
-  #   it 'should encrypt card information' do
-  #     enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-  #     _(enc).wont_equal @cc.to_s
-  #     _(enc).wont_be_nil
-  #   end
-
-  #   it 'should decrypt text' do
-  #     enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
-  #     dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
-  #     _(dec).must_equal @cc.to_s
-  #   end
-  # end
-
-  # describe 'Using Permutation cipher' do
-  #   it 'should encrypt card information' do
-  #     enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-  #     _(enc).wont_equal @cc.to_s
-  #     _(enc).wont_be_nil
-  #   end
-
-  #   it 'should decrypt text' do
-  #     enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
-  #     dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
-  #     _(dec).must_equal @cc.to_s
-  #   end
-  # end
-
   before do
     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
                          'Soumya Ray', 'Visa')
-    @key = [[3, 1, 2, 0], [2, 3, 0, 1]]
+    @key = 3
   end
 
-  # TODO: Add tests for double transposition and modern symmetric key ciphers
-  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
-  describe 'Using Double Transposition Cipher' do
+  describe 'Using Caesar cipher' do
     it 'should encrypt card information' do
-      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
       _(enc).wont_equal @cc.to_s
       _(enc).wont_be_nil
     end
 
     it 'should decrypt text' do
-      enc = DoubleTranspositionCipher.encrypt(@cc, @key)
-      dec = DoubleTranspositionCipher.decrypt(enc, @key)
+      enc = SubstitutionCipher::Caesar.encrypt(@cc, @key)
+      dec = SubstitutionCipher::Caesar.decrypt(enc, @key)
       _(dec).must_equal @cc.to_s
     end
   end
+
+  describe 'Using Permutation cipher' do
+    it 'should encrypt card information' do
+      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+      _(enc).wont_equal @cc.to_s
+      _(enc).wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      enc = SubstitutionCipher::Permutation.encrypt(@cc, @key)
+      dec = SubstitutionCipher::Permutation.decrypt(enc, @key)
+      _(dec).must_equal @cc.to_s
+    end
+  end
+
+  # TODO: Add tests for double transposition and modern symmetric key ciphers
+  #       Can you DRY out the tests using metaprogramming? (see lecture slide)
+#   before do
+#     @cc = CreditCard.new('4916603231464963', 'Mar-30-2020',
+#                          'Soumya Ray', 'Visa')
+#     @key = [[3, 1, 2, 0], [2, 3, 0, 1]]
+#   end
+
+#   describe 'Using Double Transposition Cipher' do
+#     it 'should encrypt card information' do
+#       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+#       _(enc).wont_equal @cc.to_s
+#       _(enc).wont_be_nil
+#     end
+
+#     it 'should decrypt text' do
+#       enc = DoubleTranspositionCipher.encrypt(@cc, @key)
+#       dec = DoubleTranspositionCipher.decrypt(enc, @key)
+#       _(dec).must_equal @cc.to_s
+#     end
+#   end
 end
--- substitution_cipher.rb
+++ substitution_cipher.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 module SubstitutionCipher
+  # Caesar encrypt and decrypt
   module Caesar
     # Encrypts document using key
     # Arguments:
@@ -7,6 +10,11 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using caesar cipher
+      number = document.to_s.split('')
+      result = number.map do |num|
+        ((num.ord + key) % 128).chr
+      end
+      result.join('')
     end
 
     # Decrypts String document using integer key
@@ -16,9 +24,15 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using caesar cipher
+      number = document.to_s.split('')
+      result = number.map do |num|
+        ((num.ord + 128 - key) % 128).chr
+      end
+      result.join('')
     end
   end
 
+  # Permutation encrypt and decrypt
   module Permutation
     # Encrypts document using key
     # Arguments:
@@ -27,6 +41,18 @@ module SubstitutionCipher
     # Returns: String
     def self.encrypt(document, key)
       # TODO: encrypt string using a permutation cipher
+      chars = document.to_s.split('')
+      # Create lookup table of characters
+      table_ord = (0..127).to_a.map(&:chr)
+      # Create lookup table by shuffling with the key
+      table_shuf = table_ord.shuffle(random: Random.new(key))
+      # Get a lookup table
+      table_lookup = Hash[table_ord.zip(table_shuf)]
+      # Encrype the array by the look up table
+      encrypt_arr = chars.map do |char|
+        table_lookup[char]
+      end
+      encrypt_arr.join('')
     end
 
     # Decrypts String document using integer key
@@ -36,6 +62,18 @@ module SubstitutionCipher
     # Returns: String
     def self.decrypt(document, key)
       # TODO: decrypt string using a permutation cipher
+      de_chars = document.to_s.split('')
+      # Create lookup table of characters
+      table_ord = (0..127).to_a.map(&:chr)
+      # Create lookup table by shuffling with the key
+      table_shuf = table_ord.shuffle(random: Random.new(key))
+      # Get a reverse lookup table
+      table_lookup_rev = Hash[table_shuf.zip(table_ord)]
+      # decrype the array by the look up table
+      decrypt_arr = de_chars.map do |char|
+        table_lookup_rev[char]
+      end
+      decrypt_arr.join('')
     end
   end
 end
